cd96ade3e372816b65303d0192191830077b2d86
Mohan Vaidya <131249018+mohanvaidya@users.noreply.github.com>
2023-10-02 22:29:37 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/myapp/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+import Square from './component/Square';
+
+function Board() {
+  const [xIsNext,setxIsNext]=useState(true);
+  const [squares,setSquares]=useState(Array(9).fill(null)); 
+
+  function handleClick(i){
+    {
+      if(squares[i]){
+        return;
+      }
+    }
+    const nextSquares=squares.slice();
+    if(xIsNext){
+      nextSquares[i]="X"
+    }
+    else{
+      nextSquares[i]="O"
+    }
+    setSquares(nextSquares);
+    setxIsNext(!xIsNext);
+  }
+   return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
+         </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+      </div>
+    </>
+  );
+}
+
+export default Board;
